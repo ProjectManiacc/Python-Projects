20a94b6b2460934b48ca8b9365dd27cb74c6f194
Piotrek Kluziok <piotrkluziok@gmail.com>
2022-08-02 17:59:20 +0200

    Created methods for Seat class

--- seat.py
+++ seat.py
@@ -1,3 +1,6 @@
+import sqlite3
+
+
 class Seat:
 
     database = 'cineba.db'
@@ -6,16 +9,46 @@ class Seat:
         pass
 
     def get_seat_id(self):
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+        SELECT seat_id FROM Seat 
+        """)
+        result = cursor.fetchall()
+
+        cursor.commit()
+        connection.close()
+
+        return result
 
     def get_price(self):
         pass
 
     def get_available(self):
-        pass
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+                SELECT seat_id FROM Seat WHERE taken=0
+                """)
+        result = cursor.fetchall()
+        cursor.commit()
+        connection.close()
 
-    def is_free(self):
-        pass
+        return result
+
+    def is_free(self,seat_id):
+        connection = sqlite3.connect(self.database)
+        cursor = connection.cursor()
+        cursor.execute("""
+                        SELECT taken FROM Seat WHERE taken=0 AND seat_id=?
+                        """,[seat_id])
+        cursor.commit()
+        result = cursor.fetchall()
+        connection.close()
+        if result == 0:
+            return True
+        else:
+            return False
 
     def is_occupied(self):
         pass
